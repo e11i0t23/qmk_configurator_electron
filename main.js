54d0fdaa1dbbba7b7a0a70d064c531bf7df26996
Elliot Powell <johnp8727@gmail.com>
2019-03-31 00:13:47 +0000

    Tidy main.js

--- main.js
+++ main.js
@@ -1,24 +1,14 @@
 const { app, BrowserWindow } = require('electron')
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let win
+let win // Global ref og window object
 
 function createWindow () {
-  // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600 })
-
-  // and load the index.html of the app.
-  win.loadURL('https://config.qmk.fm/')
-
-  // Open the DevTools.
+  win = new BrowserWindow({ width: 1200, height: 800 })
+  win.loadURL('http://localhost:8080/flash')
+  //win.loadFile('./dist/index.html')
   win.webContents.openDevTools()
-
   // Emitted when the window is closed.
   win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     win = null
   })
 }
@@ -44,6 +34,3 @@ app.on('activate', () => {
     createWindow()
   }
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
