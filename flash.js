1f2b3d4d5b678ff1b7cf92732e4b429a36b58c24
Elliot Powell <elliot@Elliots-iMac.local>
2019-04-05 22:07:21 +0100

    USB Flashing start

--- flash.js
+++ flash.js
@@ -3,6 +3,32 @@ const fs = require('fs');
 const { exec } = require('child_process');
 const path = require('path')
 const https = require('https')
+const usb = require('usb')
+//const usbDevices = require('./usbDevices')
+var dfuProgrammer = path.resolve('dfu/dfuprogrammer')
+
+if (process.platform == "win32") {
+  dfuProgrammer =  '"' + dfuProgrammer + '.exe"' 
+}
+
+const usbDevices = {
+  1003: {  // Atmel Corp., list sourced from http://www.linux-usb.org/usb.ids
+    12270: ['atmega8u2'],
+    12271: ['atmega16u2'],
+    12272: ['atmega32u2'],
+    12273: ['at32uc3a3'],
+    12275: ['atmega16u4'],
+    12276: ['atmega32u4'],
+    12278: ['at32uc3b0', 'at32uc3b1'],
+    12279: ['at90usb82'],
+    12280: ['at32uc3a0', 'at32uc3a1'],
+    12281: ['at90usb646'],
+    12282: ['at90usb162'],
+    12283: ['at90usb1286', 'at90usb1287'],
+    12285: ['at89c5130', 'at89c5131'],
+    12287: ['at89c5132', 'at89c5snd1c'],
+  }
+}
 
 temp.track()
 
@@ -15,14 +41,46 @@ function flashURL(url, keyboard, filename){
       response.pipe(file);
       file.on('finish', function() {
         file.close();  // close() is async, call cb after close completes.
+        console.log(inputPath)
+        checkForBoard(inputPath)
       });
     }).on('error', function(err) { // Handle errors
       fs.unlink(imputPath); // Delete the file async. (But we don't check the result)
     });
-
-    })
+  })
 }
 
 module.exports = {
   flashURL
+}
+
+function checkForBoard(inputPath) {
+  dfu_device = null;
+  for (let device of usb.getDeviceList()) {
+    if (usbDevices.hasOwnProperty(device.deviceDescriptor.idVendor) && (usbDevices[device.deviceDescriptor.idVendor].hasOwnProperty(device.deviceDescriptor.idProduct))) {
+      dfu_device = usbDevices[device.deviceDescriptor.idVendor][device.deviceDescriptor.idProduct];
+      console.log('Found atmel device: '+dfu_device[0]);
+      console.log(device)
+      flashHex(inputPath, dfu_device)
+      break; // First match wins for now
+    }
+  }
+}
+
+function flashHex(file, dfu_device){
+  command = 'dfu-programmer ' + dfu_device + ' erase --force'
+  exec(command, function(error, stdout, stderr) {
+    console.log('stdout');
+    console.log(stdout);
+    window.Bridge.statusAppend(stdout);
+    console.log('stderr');
+    console.log(stderr);
+    window.Bridge.statusAppend(stderr);
+    const regex = /.*Success.*\r?\n|\rChecking memory from .* Empty.*/;
+    if (regex.test(stderr)) {
+      callback(true);
+    } else {
+      callback(false);
+    }
+  });
 }
