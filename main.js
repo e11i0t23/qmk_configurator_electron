4dca8eada2ed0e6b4729c80761fc67aa7398e5b0
Elliot Powell <johnp8727@gmail.com>
2019-04-15 23:22:27 +0100

    fix formatting

--- main.js
+++ main.js
@@ -1,43 +1,43 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
-let win // Global ref og window object
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+let win; // Global ref og window object
 
-function createWindow () {
+function createWindow() {
   win = new BrowserWindow({
-  width: 1200,
-  height: 800,
-  webPreferences: {
-    nodeIntegration: false,
-    preload: path.join(__dirname, 'preload.js')
-  }
-})
-  win.loadURL('http://localhost:8080')
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      preload: path.join(__dirname, "preload.js")
+    }
+  });
+  win.loadURL("http://localhost:8080");
   //win.loadFile('./dist/index.html')
-  win.webContents.openDevTools()
+  win.webContents.openDevTools();
   // Emitted when the window is closed.
-  win.on('closed', () => {
-    win = null
-  })
+  win.on("closed", () => {
+    win = null;
+  });
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on("ready", createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', () => {
+app.on("window-all-closed", () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
+  if (process.platform !== "darwin") {
+    app.quit();
   }
-})
+});
 
-app.on('activate', () => {
+app.on("activate", () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (win === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
--- preload.js
+++ preload.js
@@ -1,7 +1,7 @@
 // in preload scripts, we have access to node.js and electron APIs
 // the remote web app will not have access, so this is safe
-const { ipcRenderer: ipc, remote } = require('electron');
-const { flashURL } = require('./flash')
+const { ipcRenderer: ipc, remote } = require("electron");
+const { flashURL } = require("./flash");
 
 init();
 
@@ -18,19 +18,19 @@ function init() {
   };
 
   // we get this message from the main process
-  ipc.on('markAllComplete', () => {
+  ipc.on("markAllComplete", () => {
     // the todo app defines this function
     //window.Bridge.markAllComplete();
-    console.log("we sent stuff not")
+    console.log("we sent stuff not");
   });
 }
 
 // the todo app calls this when the todo count changes
 function setDockBadge(count) {
-  console.log("yay")
-  console.log(count)
-  if(process.platform === 'darwin') {
+  console.log("yay");
+  console.log(count);
+  if (process.platform === "darwin") {
     //Coerce count into a string. Passing an empty string makes the badge disappear.
-    remote.app.dock.setBadge('' + (count || ''));
+    remote.app.dock.setBadge("" + (count || ""));
   }
 }
