a52db91b6e66c1e9d6729a973f0929f2d4aaff42
zekth <vince.legoff@gmail.com>
2019-11-14 15:31:56 +0100

    fix clean release

--- scripts/clean-release.js
+++ scripts/clean-release.js
@@ -1,2 +1,11 @@
 const rimraf = require('rimraf').sync;
-rimraf('release/!(*.zip)');
+const {readdirSync, statSync} = require('fs');
+const {join} = require('path');
+
+const dirs = (p) =>
+  readdirSync(p).filter((f) => {
+    return statSync(join(p, f)).isDirectory();
+  });
+dirs('release').forEach((p) => {
+  rimraf(join('release', p));
+});
